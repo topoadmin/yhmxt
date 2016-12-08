f6e05918a6d99b410213cea4b9ea3e2b04e3a944
Gaoshi <13701378834@163.com>
2016-12-08 18:50:15 +0800

    Revert "新增消费页面"
    This reverts commit 122099b7224c4730f24e3f06a33a6b28cbd43803.

--- dist/static/js/mock-data.js
+++
@@ -1,9 +0,0 @@
-Mock.mock('config', returnConfig())
-
-function returnConfig() {
-	return {
-		"WEB_TITLE": "短信激活码管理系统",
-		"WEB_KEYWORDS": "短信激活码,激活码,激活码系统",
-		"WEB_DESCRIPTION": "短信激活码管理系统"
-	}
-}
